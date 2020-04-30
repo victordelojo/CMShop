8dd787533e0201e75f3c9a2b13ca951ee9fa8b57
Admin <victordelojo27@gmail.com>
2020-04-30 15:34:58 +0200

    inplementación para windows

--- router/index.js
+++ router/index.js
@@ -2,14 +2,16 @@ var express = require('express');
 var router = express.Router();
 var admin = require("../controller_db/Admin.js")
 var fs = require("fs")
+var os = require("os");
+
 
 
 // Vamos a requerir del modulo que provee Node.js 
 // llamado child_process
 var exec = require('child_process').exec, child;
-// Creamos la función y pasamos el string pwd 
-// que será nuestro comando a ejecutar
-child = exec('ls ./views/default | grep .pug',
+
+if(os.platform()=="linux"){
+    child = exec('ls ./views/default | grep .pug',
 // Pasamos los parámetros error, stdout la salida 
 // que mostrara el comando
   function (error, stdout, stderr) {
@@ -46,5 +48,44 @@ child = exec('ls ./views/default | grep .pug',
       console.log('exec error: ' + error);
     }
 });
+}else{
+    child = exec('dir ./views/default/*.pug',
+// Pasamos los parámetros error, stdout la salida 
+// que mostrara el comando
+  function (error, stdout, stderr) {
+    // Imprimimos en pantalla con console.log
+    var arra=stdout.split(".pug\n")
+    console.log(arra)
+    arra.forEach(element => {
+        if(element!==""){
+            if(element=="index"){
+                element=""
+            }
+            router.get("/"+element, async function (req, res) {
+                if (fs.existsSync(__dirname + "/../CONFIGURE.json")) {
+                    var DB_CONF = require("../CONFIGURE.json")//Carga la configuración de la base de datos
+                    var url = 'mongodb://' + DB_CONF.db_user + ':' + DB_CONF.db_pass + '@' + DB_CONF.db_direccion + ':' + DB_CONF.db_port + '?authMechanism=DEFAULT&authSource=' + DB_CONF.db_auth + '';
+                    var usuAdmin = new admin(url, DB_CONF.db_name);
+                    if (await usuAdmin.comprobarInicio().catch(function () { return false })) {
+                        if(element==""){
+                            element="index"
+                        }
+                        res.render("./default/"+element+".pug");// Envia el archivo que se va a visualizar
+                    } else {
+                        res.render("./admin/configurar_CMShop.pug")
+                    }
+                } else {
+                    res.render("./admin/configurar_CMShop.pug")
+                }
+            });
+        }
+        
+    });
+    // controlamos el error
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+});
+}
 
 module.exports = router;
